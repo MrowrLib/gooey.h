5000419adb2cce3012a7c3ec1a69376ea7a95a16
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-11 13:11:38 -0700

    Load Windows.h last

--- include/UserInterface/Autodetect.h
+++ include/UserInterface/Autodetect.h
@@ -1,3 +1,4 @@
+#pragma once
 
 #if __has_include(<QApplication>)
     #include "Qt.h"
@@ -5,10 +6,8 @@
 
 #if __has_include(<wx/wx.h>)
     #include "wxWidgets.h"
-UserInterface_RegisterToolkit(wxWidgets, UserInterface::wxWidgets::Toolkit);
 #endif
 
 #if __has_include(<nana/gui.hpp>)
     #include "Nana.h"
-UserInterface_RegisterToolkit(Nana, UserInterface::Nana::Toolkit);
 #endif
--- include/UserInterface/wxWidgets.h
+++ include/UserInterface/wxWidgets.h
@@ -5,9 +5,6 @@
 // Include the default implementation first (has UserInterface_GetAPI macro)
 #include "UserInterface/DefaultImplementation.h"
 
-// Helper for UI apps:
-#include "UserInterface/UI_Main.h"
-
 // Load GUI backend
 #include "UserInterface/Backends/wxWidgets.h"
 
