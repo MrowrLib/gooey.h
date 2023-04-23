b50ef9213710c07fbdd21e0130f38829634c5c21
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-23 11:43:49 -0700

    xmake options for --nana and --wx

--- xmake.lua
+++ xmake.lua
@@ -1,31 +1,55 @@
+add_rules("mode.debug", "mode.release")
+
 set_languages("c++20")
-add_rules("mode.debug")
 set_toolchains("msvc")
-set_arch("x86")
+
+option("nana")
+    set_default(false)
+    set_showmenu(true)
+    set_description("Build Nana Example")
+option_end()
+
+option("wx")
+    set_default(false)
+    set_showmenu(true)
+    set_description("Build wxWidgets Example")
+option_end()
 
 add_repositories("MrowrLib https://github.com/MrowrLib/Packages.git")
 add_requires("string_format")
 
--- [wxWidgets]
-local vcpkg_triplet = "x86-windows-static"
-local vcpkg_root = os.getenv("VCPKG_ROOT")
-add_requires("vcpkg::wxwidgets", {system = false, configs = {triplet = vcpkg_triplet}})
-
-target("UserInterface")
+target("MicroUI")
     set_kind("headeronly")
     add_headerfiles("include/(**.h)")
-    add_includedirs("include", {public = true})
+    add_includedirs("include", { public = true })
+target_end()
 
-target("Example")
-    set_kind("binary")
-    add_ldflags("/subsystem:windows")
-    add_ldflags("/entry:WinMainCRTStartup", {force = true})
-    add_files("Example.cpp")
-    add_deps("UserInterface")
-    add_packages("string_format")
+local function example_target(name)
+    target(name)
+        set_kind("binary")
+        add_files("Example.cpp")
+        add_deps("MicroUI")
+        add_packages("string_format")
+        add_ldflags("/subsystem:windows")
+        add_ldflags("/entry:WinMainCRTStartup", { force = true })
+end
 
-    -- [wxWidgets]
-    add_packages("vcpkg::wxwidgets")
-    add_includedirs(path.join(vcpkg_root, "installed", vcpkg_triplet, "include"))
-    add_linkdirs(path.join(vcpkg_root, "installed", vcpkg_triplet, "lib"))
-    add_links("kernel32", "user32", "gdi32", "comdlg32", "winspool", "winmm", "shell32", "comctl32", "ole32", "oleaut32", "uuid", "rpcrt4", "advapi32", "wsock32", "wininet", "libpng16", "nanosvg", "nanosvgrast", "zlib")
+if get_config("nana") == true then
+    add_requires("nana")
+    example_target("nana_Example")
+        add_packages("nana")
+    target_end()
+end
+
+if get_config("wx") == true then
+    local vcpkg_triplet = get_config("arch") .. "-windows-static"
+    local vcpkg_root = os.getenv("VCPKG_ROOT")
+    add_requires("vcpkg::wxwidgets", { system = false, configs = { triplet = vcpkg_triplet } })
+    example_target("wx_Example")
+        add_packages("vcpkg::wxwidgets")
+        add_includedirs(path.join(vcpkg_root, "installed", vcpkg_triplet, "include"))
+        add_linkdirs(path.join(vcpkg_root, "installed", vcpkg_triplet, "lib"))
+        add_links("kernel32", "user32", "gdi32", "comdlg32", "winspool", "winmm", "shell32", "comctl32", "ole32", "oleaut32", "uuid", "rpcrt4", "advapi32", "wsock32", "wininet", "libpng16", "nanosvg", "nanosvgrast", "zlib")
+    target_end()
+end
+
