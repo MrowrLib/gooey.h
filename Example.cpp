dad50030626781e8e42c7daaf46df9fd06a8ee2d
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-24 20:04:22 -0700

    Support for Nana and wx (both xmake and CMake)

--- Example.cpp
+++ Example.cpp
@@ -15,35 +15,34 @@ UI_Main {
     auto* ui  = MicroUI::GetAPI();
     auto* app = ui->GetApplication();
     window    = app->NewWindow("Window 1");
-    // window2   = app->NewWindow("Window 2");
-    // window->SetHeight(800);
-    // window->SetWidth(800);
+    window2   = app->NewWindow("Window 2");
+    window->SetHeight(800);
+    window->SetWidth(800);
 
-    // window->AddLabel("Hi, I am a label!");
-    // textbox = window->AddTextbox("Butts.");
-    // window->AddButton("I am button! CLICK ME", [](auto*) {
-    //     textbox->SetText("The button was pressed!");
-    // });
+    window->AddLabel("Hi, I am a label!");
+    textbox = window->AddTextbox("Butts.");
+    window->AddButton("I am button! CLICK ME", [](auto*) {
+        textbox->SetText("The button was pressed!");
+    });
 
-    // window->AddButton("Show second window", [](auto*) { window2->Show(); });
-    // window->AddButton("Hide second window", [](auto*) { window2->Hide(); });
+    window->AddButton("Show second window", [](auto*) { window2->Show(); });
+    window->AddButton("Hide second window", [](auto*) { window2->Hide(); });
 
-    // tab1       = window->AddTab("Tab 1");
-    // auto* tab2 = window->AddTab("Tab 2");
+    tab1       = window->AddTab("Tab 1");
+    auto* tab2 = window->AddTab("Tab 2");
 
-    // tab1->AddLabel("Tab 1 label!");
-    // tab1_textbox = tab1->AddTextbox("Tab 1 textbox.");
-    // tab1->AddButton("Tab 1 button", [](auto* button) {
-    //     tab1_textbox->SetText(string_format("The {} button was pressed!",
-    //     button->GetText()).c_str()
-    //     );
-    // });
+    tab1->AddLabel("Tab 1 label!");
+    tab1_textbox = tab1->AddTextbox("Tab 1 textbox.");
+    tab1->AddButton("Tab 1 button", [](auto* button) {
+        tab1_textbox->SetText(string_format("The {} button was pressed!", button->GetText()).c_str()
+        );
+    });
 
-    // tab2->AddLabel("Butts!");
-    // tab2_textbox = tab2->AddTextbox("Butts.");
-    // tab2->AddButton("I am BUTTS! CLICK ME", [](auto*) {
-    //     tab2_textbox->SetText("The BUTTS was pressed!");
-    // });
+    tab2->AddLabel("Butts!");
+    tab2_textbox = tab2->AddTextbox("Butts.");
+    tab2->AddButton("I am BUTTS! CLICK ME", [](auto*) {
+        tab2_textbox->SetText("The BUTTS was pressed!");
+    });
 
     window->Show();
     app->Run();
--- xmake.lua
+++ xmake.lua
@@ -3,20 +3,35 @@ add_rules("mode.debug", "mode.release")
 set_languages("c++20")
 set_toolchains("msvc")
 
-option("nana")
-    set_default(false)
+option("toolkit")
+    set_default("nana")
     set_showmenu(true)
-    set_description("Build Nana Example")
+    set_values("nana", "wx")
+    set_description("Select UI Toolkit (nana or wx)")
 option_end()
 
-option("wx")
-    set_default(false)
-    set_showmenu(true)
-    set_description("Build wxWidgets Example")
-option_end()
+if has_config("arch") then
+    vcpkg_triplet = get_config("arch") .. "-windows-static"
+    vcpkg_root = os.getenv("VCPKG_ROOT")
+end
 
 add_repositories("MrowrLib https://github.com/MrowrLib/Packages.git")
 add_requires("string_format")
+
+if get_config("toolkit") == "nana" then
+    add_requires("nana")
+end
+
+if get_config("toolkit") == "wx" then
+    add_requires("vcpkg::wxwidgets", { system = false, configs = { triplet = vcpkg_triplet } })
+end
+
+if get_config("toolkit") == "" then
+    print("Must choose a toolkit with `xmake configure --toolkit=<nana|wx>`")
+    return
+elseif not get_config("toolkit") == nil then
+    print(string.format("Selected UI toolkit: '%s'", get_config("toolkit")))
+end
 
 target("MicroUI")
     set_kind("headeronly")
@@ -24,32 +39,22 @@ target("MicroUI")
     add_includedirs("include", { public = true })
 target_end()
 
-local function example_target(name)
-    target(name)
-        set_kind("binary")
-        add_files("Example.cpp")
-        add_deps("MicroUI")
-        add_packages("string_format")
-        add_ldflags("/subsystem:windows")
-        add_ldflags("/entry:WinMainCRTStartup", { force = true })
+target("Example")
+    set_kind("binary")
+    add_files("Example.cpp")
+    add_deps("MicroUI")
+    add_packages("string_format")
+    add_ldflags("/subsystem:windows")
+    add_ldflags("/entry:WinMainCRTStartup", { force = true })
+
+if get_config("toolkit") == "nana" then
+    add_packages("nana")
 end
 
-if get_config("nana") == true then
-    add_requires("nana")
-    example_target("nana_Example")
-        add_packages("nana")
-    target_end()
-end
-
-if get_config("wx") == true then
-    local vcpkg_triplet = get_config("arch") .. "-windows-static"
-    local vcpkg_root = os.getenv("VCPKG_ROOT")
-    add_requires("vcpkg::wxwidgets", { system = false, configs = { triplet = vcpkg_triplet } })
-    example_target("wx_Example")
-        add_packages("vcpkg::wxwidgets")
-        add_includedirs(path.join(vcpkg_root, "installed", vcpkg_triplet, "include"))
-        add_linkdirs(path.join(vcpkg_root, "installed", vcpkg_triplet, "lib"))
-        add_links("kernel32", "user32", "gdi32", "comdlg32", "winspool", "winmm", "shell32", "comctl32", "ole32", "oleaut32", "uuid", "rpcrt4", "advapi32", "wsock32", "wininet", "libpng16", "nanosvg", "nanosvgrast", "zlib")
-    target_end()
+if get_config("toolkit") == "wx" then
+    add_packages("vcpkg::wxwidgets")
+    add_includedirs(path.join(vcpkg_root, "installed", vcpkg_triplet, "include"))
+    add_linkdirs(path.join(vcpkg_root, "installed", vcpkg_triplet, "lib"))
+    add_links("kernel32", "user32", "gdi32", "comdlg32", "winspool", "winmm", "shell32", "comctl32", "ole32", "oleaut32", "uuid", "rpcrt4", "advapi32", "wsock32", "wininet", "libpng16", "nanosvg", "nanosvgrast", "zlib")
 end
 
