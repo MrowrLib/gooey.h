a18cc3b56b486f67d7b4ac7e6b9a7779b25bb52f
Mrowr Purr <mrowr.purr@gmail.com>
2023-05-03 08:51:04 -0700

    Background image (panel only right now)

--- Gooey.FLTK/include/gooey/FLTKAdapter/Panel.h
+++ Gooey.FLTK/include/gooey/FLTKAdapter/Panel.h
@@ -9,24 +9,103 @@
 #include <FL/Fl_Group.H>
 #include <FL/Fl_Output.H>
 #include <FL/Fl_Pack.H>
+#include <FL/fl_draw.H>
 #include <gooey.h>
+
+#include <memory>
 
 #include "Colors.h"
 #include "WidgetContainer.h"
 
 namespace gooey::FLTKAdapter {
 
+    namespace Impl {
+        class BackgroundImageBox : public Fl_Box {
+            std::unique_ptr<Fl_PNG_Image> _image;
+
+        public:
+            BackgroundImageBox(int x, int y, int w, int h) : Fl_Box(x, y, w, h) {
+                // box(FL_FLAT_BOX);
+                // end();
+            }
+
+            void SetBackgroundImage(const char* path) {
+                _image = std::make_unique<Fl_PNG_Image>(path);
+            }
+
+            void draw() override {
+                // ONCE
+                // if (_image) _image->draw(x(), y(), w(), h());
+
+                // Repeating
+                // if (_image) {
+                //     fl_push_clip(x(), y(), w(), h());
+                //     int imgW = _image->w();
+                //     int imgH = _image->h();
+                //     for (int X = x(); X < x() + w(); X += imgW) {
+                //         for (int Y = y(); Y < y() + h(); Y += imgH) {
+                //             _image->draw(X, Y, imgW, imgH, 0, 0);
+                //         }
+                //     }
+                //     fl_pop_clip();
+                // }
+
+                // Stretch (maintain aspect ratio)
+                // if (_image) {
+                //     fl_push_clip(x(), y(), w(), h());
+
+                //     // Calculate the aspect ratio of the image
+                //     float imgAspectRatio =
+                //         static_cast<float>(_image->w()) / static_cast<float>(_image->h());
+                //     int drawW = w();
+                //     int drawH = static_cast<int>(drawW / imgAspectRatio);
+
+                //     // If the calculated height is less than the available height, adjust the
+                //     width
+                //     // accordingly
+                //     if (drawH < h()) {
+                //         drawH = h();
+                //         drawW = static_cast<int>(drawH * imgAspectRatio);
+                //     }
+
+                //     _image->draw(x(), y(), drawW, drawH, 0, 0);
+
+                //     fl_pop_clip();
+                // }
+
+                // Stetch (ignore aspect ratio)
+                if (_image) {
+                    fl_push_clip(x(), y(), w(), h());
+                    std::unique_ptr<Fl_Image> scaledImage(_image->copy(w(), h()));
+                    scaledImage->draw(x(), y());
+                    fl_pop_clip();
+                }
+
+                Fl_Box::draw();
+            }
+        };
+    }
+
     class Panel : public UIPanel, WidgetContainer {
-        Fl_Group* _implGroup;
-        Fl_Pack*  _implPack;
+        Fl_Group*                 _implGroup;
+        Fl_Pack*                  _implPack;
+        Impl::BackgroundImageBox* _implBackgroundImageBox;
 
     public:
         Panel(Fl_Pack* pack, bool horizontal)
             : _implGroup(new Fl_Group(0, 0, DefaultSizes::PanelWidth, DefaultSizes::PanelHeight)) {
             _implGroup->box(FL_UP_BOX);
+
+            _implBackgroundImageBox = new Impl::BackgroundImageBox(
+                0, 0, DefaultSizes::PanelWidth, DefaultSizes::PanelHeight
+            );
+            _implBackgroundImageBox->SetBackgroundImage("Logo.png");
+            _implGroup->add(_implBackgroundImageBox);
+
             _implPack = new Fl_Pack(0, 0, DefaultSizes::PanelWidth, DefaultSizes::PanelHeight);
             _implPack->type(horizontal ? Fl_Pack::HORIZONTAL : Fl_Pack::VERTICAL);
             _implPack->spacing(10);  // Remove me
+
             // _implPack->end();
             _implGroup->end();
             SetImplFlPack(_implPack);
--- Gooey/include/gooey.h
+++ Gooey/include/gooey.h
@@ -2,135 +2,144 @@
 
 namespace gooey {
 
-    struct UIComponent {
-        // virtual bool IsDetached()                        = 0;
-        // virtual void Detach()                                  = 0;
-        // virtual void OnDetatch(void (*callback)(UIComponent*)) = 0;
-        virtual bool SetBackgroundColor(unsigned int red, unsigned int green, unsigned int blue) {
-            return false;
-        }
-        virtual bool SetForegroundColor(unsigned int red, unsigned int green, unsigned int blue) {
-            return false;
-        }
-    };
+    extern "C" {
+        struct UIComponent {
+            // virtual bool IsDetached()                        = 0;
+            // virtual void Detach()                                  = 0;
+            // virtual void OnDetatch(void (*callback)(UIComponent*)) = 0;
+            virtual bool SetBackgroundColor(
+                unsigned int red, unsigned int green, unsigned int blue
+            ) {
+                return false;
+            }
+            virtual bool SetForegroundColor(
+                unsigned int red, unsigned int green, unsigned int blue
+            ) {
+                return false;
+            }
+        };
 
-    struct UIWidget;
+        struct UIWidget;
 
-    struct UICommonEvents {
-        virtual bool OnMouseEnter(void (*callback)(UIWidget*)) { return false; }
-        virtual bool OnMouseLeave(void (*callback)(UIWidget*)) { return false; }
-        virtual bool OnLeftClick(void (*callback)(UIWidget*)) { return false; }
-        virtual bool OnRightClick(void (*callback)(UIWidget*)) { return false; }
-        virtual bool OnMiddleClick(void (*callback)(UIWidget*)) { return false; }
-        virtual bool OnLeftDoubleClick(void (*callback)(UIWidget*)) { return false; }
-        virtual bool OnRightDoubleClick(void (*callback)(UIWidget*)) { return false; }
-        virtual bool OnMiddleDoubleClick(void (*callback)(UIWidget*)) { return false; }
-    };
+        struct UICommonEvents {
+            virtual bool OnMouseEnter(void (*callback)(UIWidget*)) { return false; }
+            virtual bool OnMouseLeave(void (*callback)(UIWidget*)) { return false; }
+            virtual bool OnLeftClick(void (*callback)(UIWidget*)) { return false; }
+            virtual bool OnRightClick(void (*callback)(UIWidget*)) { return false; }
+            virtual bool OnMiddleClick(void (*callback)(UIWidget*)) { return false; }
+            virtual bool OnLeftDoubleClick(void (*callback)(UIWidget*)) { return false; }
+            virtual bool OnRightDoubleClick(void (*callback)(UIWidget*)) { return false; }
+            virtual bool OnMiddleDoubleClick(void (*callback)(UIWidget*)) { return false; }
+        };
 
-    struct UIWidget : public UIComponent, public UICommonEvents {
-        virtual bool Show() { return false; }
-        virtual bool Hide() { return false; }
-    };
+        struct UIWidget : public UIComponent, public UICommonEvents {
+            virtual bool Show() { return false; }
+            virtual bool Hide() { return false; }
+        };
 
-    // struct UISpacer : public UIWidget {
-    //     virtual ~UISpacer() = default;
-    // };
+        // struct UISpacer : public UIWidget {
+        //     virtual ~UISpacer() = default;
+        // };
 
-    struct UIImage : public UIWidget {};
+        struct UIImage : public UIWidget {};
 
-    struct UILabel : public UIWidget {
-        virtual ~UILabel() = default;
-        virtual bool        SetText(const char* text) { return false; }
-        virtual const char* GetText() { return nullptr; }
-    };
+        struct UILabel : public UIWidget {
+            virtual ~UILabel() = default;
+            virtual bool        SetText(const char* text) { return false; }
+            virtual const char* GetText() { return nullptr; }
+        };
 
-    struct UIButton : public UIWidget {
-        virtual ~UIButton() = default;
-        virtual bool        SetText(const char* text) { return false; }
-        virtual const char* GetText() { return nullptr; }
-        virtual bool        OnButtonPress(void (*callback)(UIWidget*)) { return false; }
-    };
+        struct UIButton : public UIWidget {
+            virtual ~UIButton() = default;
+            virtual bool        SetText(const char* text) { return false; }
+            virtual const char* GetText() { return nullptr; }
+            virtual bool        OnButtonPress(void (*callback)(UIWidget*)) { return false; }
+        };
 
-    struct UIPanel;
-    struct UIGrid;
+        struct UIPanel;
+        struct UIGrid;
 
-    struct UIWidgetContainer {
-        virtual ~UIWidgetContainer() = default;
-        virtual size_t    GetWidgetCount() { return 0; }
-        virtual UIWidget* GetWidget(size_t index) { return nullptr; }
-        virtual UIPanel*  AddHorizontalPanel() { return nullptr; }
-        virtual UIPanel*  AddVerticalPanel() { return nullptr; }
-        virtual UIGrid*   AddGrid(unsigned int cols, unsigned int rows) { return nullptr; }
-        virtual UILabel*  AddLabel(const char* text) { return nullptr; }
-        virtual UIButton* AddButton(const char* text) { return nullptr; }
-        virtual UIImage*  AddImage(const char* path) { return nullptr; }
-        // virtual UISpacer* AddSpacer(unsigned int size) { return nullptr; }
-    };
+        struct UIWidgetContainer {
+            virtual ~UIWidgetContainer() = default;
+            virtual size_t    GetWidgetCount() { return 0; }
+            virtual UIWidget* GetWidget(size_t index) { return nullptr; }
+            virtual UIPanel*  AddHorizontalPanel() { return nullptr; }
+            virtual UIPanel*  AddVerticalPanel() { return nullptr; }
+            virtual UIGrid*   AddGrid(unsigned int cols, unsigned int rows) { return nullptr; }
+            virtual UILabel*  AddLabel(const char* text) { return nullptr; }
+            virtual UIButton* AddButton(const char* text) { return nullptr; }
+            virtual UIImage*  AddImage(const char* path) { return nullptr; }
+            // virtual UISpacer* AddSpacer(unsigned int size) { return nullptr; }
+        };
 
-    struct UIGrid : public UIWidget, public UIWidgetContainer {
-        virtual UIWidget* GetWidget(unsigned int x, unsigned int y) { return nullptr; }
-        virtual UIPanel*  AddHorizontalPanel(
-             unsigned int x, unsigned int y, unsigned int cols, unsigned int rows
-         ) {
-            return nullptr;
-        }
-        virtual UIPanel* AddVerticalPanel(
-            unsigned int x, unsigned int y, unsigned int cols, unsigned int rows
-        ) {
-            return nullptr;
-        }
-        virtual UIGrid* AddGrid(
-            unsigned int cols, unsigned int rows, unsigned int x, unsigned int y,
-            unsigned int childCols, unsigned int childRows
-        ) {
-            return nullptr;
-        }
-        virtual UILabel* AddLabel(
-            const char* text, unsigned int x, unsigned int y, unsigned int cols, unsigned int rows
-        ) {
-            return nullptr;
-        }
-        virtual UIButton* AddButton(
-            const char* text, unsigned int x, unsigned int y, unsigned int cols, unsigned int rows
-        ) {
-            return nullptr;
-        }
-        virtual UIImage* AddImage(
-            const char* path, unsigned int x, unsigned int y, unsigned int cols, unsigned int rows
-        ) {
-            return nullptr;
-        }
-    };
+        struct UIGrid : public UIWidget, public UIWidgetContainer {
+            virtual UIWidget* GetWidget(unsigned int x, unsigned int y) { return nullptr; }
+            virtual UIPanel*  AddHorizontalPanel(
+                 unsigned int x, unsigned int y, unsigned int cols, unsigned int rows
+             ) {
+                return nullptr;
+            }
+            virtual UIPanel* AddVerticalPanel(
+                unsigned int x, unsigned int y, unsigned int cols, unsigned int rows
+            ) {
+                return nullptr;
+            }
+            virtual UIGrid* AddGrid(
+                unsigned int cols, unsigned int rows, unsigned int x, unsigned int y,
+                unsigned int childCols, unsigned int childRows
+            ) {
+                return nullptr;
+            }
+            virtual UILabel* AddLabel(
+                const char* text, unsigned int x, unsigned int y, unsigned int cols,
+                unsigned int rows
+            ) {
+                return nullptr;
+            }
+            virtual UIButton* AddButton(
+                const char* text, unsigned int x, unsigned int y, unsigned int cols,
+                unsigned int rows
+            ) {
+                return nullptr;
+            }
+            virtual UIImage* AddImage(
+                const char* path, unsigned int x, unsigned int y, unsigned int cols,
+                unsigned int rows
+            ) {
+                return nullptr;
+            }
+        };
 
-    struct UIPanel : public UIWidget, public UIWidgetContainer {
-        virtual ~UIPanel() = default;
-    };
+        struct UIPanel : public UIWidget, public UIWidgetContainer {
+            virtual ~UIPanel() = default;
+        };
 
-    struct UITab : public UIWidgetContainer {};
+        struct UITab : public UIWidgetContainer {};
 
-    struct UIWindow : public UIComponent, public UIWidgetContainer, public UICommonEvents {
-        virtual ~UIWindow() = default;
-        // virtual bool OnClick(void (*callback)(UIWindow*)) { return false; }
-        // virtual bool OnMouseEnter(void (*callback)(UIWindow*)) { return false; }
-        // virtual bool OnMouseLeave(void (*callback)(UIWindow*)) { return false; }
-        // virtual bool OnClose(void (*callback)(UIWindow*)) { return false; }
-        // virtual bool OnHide(void (*callback)(UIWindow*)) { return false; }
-        // virtual bool OnShow(void (*callback)(UIWindow*)) { return false; }
-        virtual bool        IsVisible() { return false; }
-        virtual bool        Show() { return false; }
-        virtual bool        Hide() { return false; }
-        virtual bool        Close() { return false; }
-        virtual bool        SetTitle(const char* title) { return false; }
-        virtual const char* GetTitle() { return nullptr; }
-        virtual bool        OnShow(void (*callback)(UIWindow*)) { return false; }
-        virtual bool        OnHide(void (*callback)(UIWindow*)) { return false; }
-        virtual bool        OnClose(void (*callback)(UIWindow*)) { return false; }
-    };
+        struct UIWindow : public UIComponent, public UIWidgetContainer, public UICommonEvents {
+            virtual ~UIWindow() = default;
+            // virtual bool OnClick(void (*callback)(UIWindow*)) { return false; }
+            // virtual bool OnMouseEnter(void (*callback)(UIWindow*)) { return false; }
+            // virtual bool OnMouseLeave(void (*callback)(UIWindow*)) { return false; }
+            // virtual bool OnClose(void (*callback)(UIWindow*)) { return false; }
+            // virtual bool OnHide(void (*callback)(UIWindow*)) { return false; }
+            // virtual bool OnShow(void (*callback)(UIWindow*)) { return false; }
+            virtual bool        IsVisible() { return false; }
+            virtual bool        Show() { return false; }
+            virtual bool        Hide() { return false; }
+            virtual bool        Close() { return false; }
+            virtual bool        SetTitle(const char* title) { return false; }
+            virtual const char* GetTitle() { return nullptr; }
+            virtual bool        OnShow(void (*callback)(UIWindow*)) { return false; }
+            virtual bool        OnHide(void (*callback)(UIWindow*)) { return false; }
+            virtual bool        OnClose(void (*callback)(UIWindow*)) { return false; }
+        };
 
-    struct UIApplication {
-        virtual ~UIApplication() = default;
-        virtual bool      Start() { return false; }
-        virtual bool      Shutdown() { return false; }
-        virtual UIWindow* AddWindow() { return nullptr; }
-    };
+        struct UIApplication {
+            virtual ~UIApplication() = default;
+            virtual bool      Start() { return false; }
+            virtual bool      Shutdown() { return false; }
+            virtual UIWindow* AddWindow() { return nullptr; }
+        };
+    }
 }
