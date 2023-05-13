d68af48e02791f3452ad60be01a3701403c918f8
Mrowr Purr <mrowr.purr@gmail.com>
2023-05-12 19:20:21 -0700

    Qt on Windows OK with xmake

--- xmake/fltk.lua
+++ xmake/fltk.lua
@@ -1,18 +1,9 @@
--- export VCPKG_ROOT="$HOME/Code/vcpkg"
--- export FLTK_INCLUDE_DIR=$(brew --prefix fltk)/include
--- export FLTK_LIB_DIR=$(brew --prefix fltk)/lib
-
 function install_ui_library_global_dependency()
-    -- I can't get any of these to work on Mac yet
-    -- add_requires("fltk")
-    -- add_requires("vcpkg::fltk")
     add_requires("brew::fltk")
 end
 
 function setup_target_for_ui_library()
     add_deps("gooey-fltk")
     add_defines("GOOEY_USE_FLTK")
-
-    -- ...
     add_packages("brew::fltk")
 end
--- xmake/qt.lua
+++ xmake/qt.lua
@@ -1,32 +1,43 @@
--- export VCPKG_ROOT="$HOME/Code/vcpkg"
--- export FLTK_INCLUDE_DIR=$(brew --prefix fltk)/include
--- export FLTK_LIB_DIR=$(brew --prefix fltk)/lib
-
 function install_ui_library_global_dependency()
-    -- I can't get any of these to work on Mac yet
-    -- add_requires("fltk")
-    -- add_requires("vcpkg::fltk")
-    add_requires("brew::fltk")
+    --
 end
 
 function setup_target_for_ui_library()
     add_deps("gooey-qt")
     add_defines("GOOEY_USE_QT")
 
-    -- ...
-    local qt_dir = path.join(os.getenv("QTROOT"), "macos")
-    local qt_lib_dir = path.join(qt_dir, "lib")
+    if is_host("windows") then
+        add_cxflags("/Zc:__cplusplus")
 
-    add_includedirs(path.join(qt_dir, "include"))
-    add_includedirs(path.join(qt_lib_dir, "QtCore.framework", "Headers"))
-    add_includedirs(path.join(qt_lib_dir, "QtGui.framework", "Headers"))
-    add_includedirs(path.join(qt_lib_dir, "QtWidgets.framework", "Headers"))
+        local qt_dir = path.join(os.getenv("QTROOT"), "msvc2019_64")
+        local qt_lib_dir = path.join(qt_dir, "lib")
+        local qt_include_dir = path.join(qt_dir, "include")
 
-    add_linkdirs(path.join(qt_lib_dir, "QtCore.framework"))
-    add_linkdirs(path.join(qt_lib_dir, "QtGui.framework"))
-    add_linkdirs(path.join(qt_lib_dir, "QtWidgets.framework"))
+        add_includedirs(path.join(qt_include_dir, "QtCore"))
+        add_includedirs(path.join(qt_include_dir, "QtGui"))
+        add_includedirs(path.join(qt_include_dir, "QtWidgets"))
 
-    add_rules("qt.application")
-    add_ldflags("-F" .. qt_lib_dir)
-    add_frameworks("QtCore", "QtGui", "QtWidgets")
+        add_linkdirs(qt_lib_dir)
+        add_links("Qt6Core", "Qt6Gui", "Qt6Widgets")
+
+        add_rules("qt.application")
+    elseif is_host("macosx") then
+        local qt_dir = path.join(os.getenv("QTROOT"), "macos")
+        local qt_lib_dir = path.join(qt_dir, "lib")
+
+        add_includedirs(path.join(qt_dir, "include"))
+        add_includedirs(path.join(qt_lib_dir, "QtCore.framework", "Headers"))
+        add_includedirs(path.join(qt_lib_dir, "QtGui.framework", "Headers"))
+        add_includedirs(path.join(qt_lib_dir, "QtWidgets.framework", "Headers"))
+
+        add_linkdirs(path.join(qt_lib_dir, "QtCore.framework"))
+        add_linkdirs(path.join(qt_lib_dir, "QtGui.framework"))
+        add_linkdirs(path.join(qt_lib_dir, "QtWidgets.framework"))
+
+        add_rules("qt.application")
+        add_ldflags("-F" .. qt_lib_dir)
+        add_frameworks("QtCore", "QtGui", "QtWidgets")
+    elseif is_host("linux") then
+        -- TODO
+    end
 end
